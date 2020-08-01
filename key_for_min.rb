a697a468ccf05b9f0023fb7fa2158dc83420e0b8
Nerly Ton <nerlyton@gmail.com>
2020-08-01 03:12:43 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,8 +6,8 @@ def key_for_min_value(name_hash)
   smallest_key = nil
   name_hash.collect do |key, value|
     if smallest_value == 0 || value < smallest_value
-      lowest_value = value
-      lowest_key = key
+      smallest_value = value
+      smallest_key = key
     end
   end
   lowest_key
